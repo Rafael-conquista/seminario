77cddfdde58399cb8a69294e711be404ee982144
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-10-02 20:31:54 -0300

    feat: Adiciona botões de restart e fechar simulação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route,Routes, BrowserRouter } from "react-router-dom";
+import SistemaSolar from './pages/sistema_solar';
+import Menu from './pages/menu'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Menu />}></Route>
+          <Route path="/sistemaSolar" element={<SistemaSolar/>}></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/menu.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import planets from '../images/sistema_solar_wallpaper.jpg'
+import planets4k from '../images/sistema_solar_wallpaper_4k.jpg'
+import './menu.css'
+
+function Menu(){
+    return(
+        <>
+        <div className='all'>
+        <img src={planets4k} alt="planets" className="tela"/>
+            <div className="paginasFisica">
+            <div>
+                <h2 className='h2'>Física aplicadas em JavaScript</h2>
+                <hr className='.hr'></hr>
+                <li className="tema">
+                <img src={planets} alt='vazio' className="tema_imagem" onClick={() => window.location.href = '/sistemaSolar'}/> 
+                <div className="legenda ">Sistema Solar</div>
+                </li>    
+                <li className="tema">
+                <img src="" className="tema_imagem" alt='vazio'/> 
+                <div className="legenda">Outros temas</div>
+                </li> 
+            </div>
+            </div>
+        </div>
+        </>
+        
+    )
+}
+
+export default Menu
